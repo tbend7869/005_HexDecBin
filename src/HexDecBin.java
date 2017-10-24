f426357247f13576fe688f4f9d883c65e157f2a1
compsci <compsci@compsci>
2017-10-24 10:50:04 -0600

    If statements added, but no conversions yet

--- src/HexDecBin.java
+++ src/HexDecBin.java
@@ -12,6 +12,8 @@ public class HexDecBin {
 	Scanner kbd = new Scanner(System.in);
 	String name = "";
 	String hex = "";
+	String base = "";
+	String convert = "";
 	int num;
 	
 	public HexDecBin() {
@@ -22,18 +24,54 @@ public class HexDecBin {
 	} //end of HexDecBin (constructor)
 	
 	public void setHexNumber() {
-		
-
-		
+		/*
 		System.out.println("\nEnter the hex number :");
 		hex = kbd.nextLine();
 		// mouse over parseInt to read pop=up guide
 		num = Integer.parseInt(hex, 16);
 		System.out.println("You have entered hex " + hex );
 		System.out.println("Decimal value is " + num + "(remember 0-" + num + " has " + (num+1) + "values)");
-		
+		*/
+		System.out.println("\nWhat is your starting base Hex, Dec, or Bin?");
+		base = kbd.nextLine();
+		System.out.println("Your base is " + base + ". What base do you want to convert to Hex, Dec or Bin?");
+		convert = kbd.nextLine();
+		System.out.println("You want to convert " + base + " to " + convert);
+		if(base == "Hex") {
+			System.out.println("Enter Hex base value: ");
+			int HValue = kbd.nextInt();
+			if(convert == "Dec") {
+				
+			} else if(convert == "Bin") {
+				
+			} else {
+				System.out.println("You didn't type one of the values listed for the value you want to convert to. Please try again.");
+			}
+		} else if (base == "Dec") {
+			System.out.println("Enter Dec base value: ");
+			int DValue = kbd.nextInt();
+			if(convert == "Hex") {
+				
+			} else if(convert == "Bin") {
+				
+			} else {
+				System.out.println("You didn't type one of the values listed for the value you want to convert to. Please try again.");
+			}
+		} else if (base == "Bin") {
+			System.out.println("Enter Bin base value: ");
+			int BValue = kbd.nextInt();
+			if(convert == "Bin") {
+				
+			} else if(convert == "Hex") {
+				
+			} else {
+				System.out.println("You didn't type one of the values listed for the value you want to convert to. Please try again.");
+			}
+		} else {
+			System.out.println("You didn't type one of the values listed for the base. Please try again.");
+		}
 	}
-	
+	/*
 	void convert() {
 		
 		String binary = Integer.toBinaryString(num);
@@ -43,5 +81,5 @@ public class HexDecBin {
 		System.out.println("Number of whole nibbles (4 bits) is x with y bits left over");
 		System.out.println("Number of whole bytes (8 bits) is x with y bits remaining");
 	}
-		
+	*/	
 }
--- src/RunHere.java
+++ src/RunHere.java
@@ -1,15 +1,27 @@
 
 public class RunHere {
+	
+	/* Grading Req's! */
+	/*
+	 * 1. All JOP's remove / comment out Scanner, syso
+	 * 2. Runnable jar -- stand alone outside of Eclipse
+	 * 3. Ask for name, repeat name with friendly "Hello ...
+	 * 4. Ask "What base are we starting with, Hex, Cec, or Bin?"
+	 * 5. Ask "What base are we converting to, Hex, Dec, or Bin?"
+	 * 6. Enter the starting number
+	 * 7. Do the conversion, display the result
+	 * Ask if they want to go again; if yes start over, if no friendly exit
+	 */
 
 		public static void main(String[] args) {
 			
 			//Added hex to bin funtionality (just for git practice)
-			BinPower binpower = new BinPower();
-			binpower.setBinary();
-			
 			HexDecBin baseConverter = new HexDecBin();
 			baseConverter.setHexNumber();
-			baseConverter.convert();
+			//baseConverter.convert();
+			
+			BinPower binpower = new BinPower();
+			binpower.setBinary();
 		}
 
 }
